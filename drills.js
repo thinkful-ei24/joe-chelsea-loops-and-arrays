bb231af844cdb810e3e6c7253c94d9884b41aed3
Joe-Pena <joepena@protonmail.com>
2018-08-21 10:50:25 -0600

    FizzBuzz

--- drills.js
+++ drills.js
@@ -42,3 +42,28 @@ const average = (numbers) => {
 testArr2 = [1, 4,3,7,10];
 
 average(testArr2);
+
+//------------------------------------------------------------
+
+//FizzBuzz
+
+
+const fizzBuzz = (countTo) => {
+    arr = [];
+    for (let i = 1; i <= countTo; i++) {
+        if (i % 3 === 0 && i % 5 ===0) {
+            arr.push('FizzBuzz');
+        }else if (i % 3 === 0) {
+            arr.push('Fizz');
+        } else if (i % 5 === 0) {
+            arr.push('Buzz');
+        } else {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+
+console.log(fizzBuzz(15));
+
+//-------------------------
